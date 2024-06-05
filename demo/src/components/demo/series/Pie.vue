c660dd89aa36c2f2d66cdce583301f39429db4a4
sdyou <upstairs0102@gmail.com>
2024-06-05 23:59:22 +0800

    wip

---
+++ demo/src/components/demo/series/Pie.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="chart"></div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+import { SeriesChart } from '../../../../../packages/orbcharts-core/src'
+import { Pie } from '../../../../../packages/orbcharts-plugins-basic/src'
+
+onMounted(() => {
+  const el = document.querySelector('#chart')
+
+  const chart = new SeriesChart(el)
+
+  const pie = new Pie()
+
+  chart.plugins$.next([pie])
+
+  chart.data$.next([15,38,25,17,26,72,64,29])
+})
+
+
+</script>
