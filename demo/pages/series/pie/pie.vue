fd230e14944e46c5977d6ec94f6dfcd5e8522053
sdyou <upstairs0102@gmail.com>
2024-06-09 00:24:04 +0800

    feat: demo改用nuxt

---
+++ demo/pages/series/pie/pie.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="chart" style="width:100%;height:100vh"></div>
+</template>
+
+<script lang="ts" setup>
+import { SeriesChart } from '../../../../packages/orbcharts-core/src'
+import { Pie } from '../../../../packages/orbcharts-plugins-basic/src'
+// import { SeriesChart } from 'orbcharts'
+// import { Pie } from 'orbcharts'
+
+onMounted(() => {
+  const el = document.querySelector('#chart')
+
+  const chart = new SeriesChart(el!)
+
+  const pie = new Pie()
+
+  chart.plugins$.next([pie])
+
+  chart.data$.next([15,38,25,17,26,72,64,29])
+})
+
+
+</script>
