7b956aedf5397be39f3037aaf64326c118a8c50f
upstairs0102 <upstairs0102@gmail.com>
2025-03-15 19:05:59 +0800

    feat: adjust demo

--- demo/pages/demo1/index.client.vue
+++ demo/pages/demo1/index.client.vue
@@ -43,15 +43,20 @@ onMounted(() => {
   // })
 
   chart.chartParams$.next({
-    padding: {
-      top: 60,
-      right: 120,
-      bottom: 60,
-      left: 120
-    },
+    // padding: {
+    //   top: 60,
+    //   right: 120,
+    //   bottom: 60,
+    //   left: 120
+    // },
     // highlightTarget: 'series'
   })
   chart!.plugins$.next([ bubbles, seriesLegend ])
+  // bubbles.params$.next({
+  //   force: {
+  //     strength: 0.1,
+  //   }
+  // })
   chart.dataFormatter$.next({
     sumSeries: false,
     separateSeries: false,
@@ -74,34 +79,56 @@ onMounted(() => {
         })
       }
       chart.dataFormatter$.next({
-        sumSeries: false,
         separateSeries: true,
-        seriesLabels: ['關鍵字', '組織團體', '地點', '人物', '企業品牌']
+        seriesLabels: ['關鍵字', '組織團體', '地點', '人物', '企業品牌'],
+        container: {
+          columnGap: 0,
+          rowGap: 0
+        }
       })
     } else if (i == 1) {
       if (j == 1) {
         chart!.plugins$.next([ rose, roseLabels, seriesLegend ])
       } else {
         chart.dataFormatter$.next({
-          sumSeries: true,
           separateSeries: true,
-          seriesLabels: ['關鍵字', '組織團體', '地點', '人物', '企業品牌']
+          separateLabel: true,
+          seriesLabels: ['關鍵字', '組織團體', '地點', '人物', '企業品牌'],
+          container: {
+            columnGap: 0,
+            rowGap: 0
+          }
         })
       }
       
     } else if (i == 2)  {
       chart.dataFormatter$.next({
         sumSeries: true,
-        separateSeries: false,
-        seriesLabels: ['關鍵字', '組織團體', '地點', '人物', '企業品牌']
+        separateSeries: true,
+        seriesLabels: ['關鍵字', '組織團體', '地點', '人物', '企業品牌'],
+        container: {
+          columnGap: 0,
+          rowGap: 0
+        }
       })
       
     } else if (i == 3)  {
       chart.dataFormatter$.next({
-        sumSeries: false,
-        separateSeries: false,
-        seriesLabels: ['關鍵字', '組織團體', '地點', '人物', '企業品牌']
+        sumSeries: true,
+        separateLabel: false,
+        seriesLabels: ['關鍵字', '組織團體', '地點', '人物', '企業品牌'],
+        container: {
+          columnGap: 0,
+          rowGap: 0
+        }
       })
+      if (j == 1) {
+        pie.params$.next({
+          innerRadius: 0.5
+        })
+        chart!.plugins$.next([ pie, pieLabels, pieEventTexts, seriesLegend ])
+        
+      }
     } else if (i == 4)  {
       chart.dataFormatter$.next({
         sumSeries: false,
@@ -110,12 +137,17 @@ onMounted(() => {
       })
       if (j == 0) {
         pie.params$.next({
-          innerRadius: 0.5
+          innerRadius: 0
         })
-        chart!.plugins$.next([ pie, pieLabels, pieEventTexts, seriesLegend ])
+        chart!.plugins$.next([ pie, pieLabels, seriesLegend ])
         
       } else if (j == 1) {
         chart!.plugins$.next([ bubbles, seriesLegend ])
+        // bubbles.params$.next({
+        //   force: {
+        //     strength: 0.1,
+        //   }
+        // })
       }
     }
 
